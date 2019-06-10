b8157f595fa63bb375af4dcecee560f833b5e193
SLoban <sviatlana.loban@gmail.com>
2019-06-10 22:42:05 +0300

    added FeedViewControllers B and C

---
+++ Week1StoryboardNavigation/FeedViewControllerB.swift
@@ -0,0 +1,21 @@
+//
+//  FeedViewControllerB.swift
+//  Week1StoryboardNavigation
+//
+//  Created by Admin on 6/10/19.
+//  Copyright © 2019 SLoban. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewControllerB: UIViewController {
+    
+    var shouldPop = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ Week1StoryboardNavigation/FeedViewControllerC.swift
@@ -0,0 +1,20 @@
+//
+//  FeedViewControllerC.swift
+//  Week1StoryboardNavigation
+//
+//  Created by Admin on 6/10/19.
+//  Copyright © 2019 SLoban. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewControllerC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
