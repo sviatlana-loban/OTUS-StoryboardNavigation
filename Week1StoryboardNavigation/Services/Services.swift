e11388355e0bb7f1ac47ebb6a9d4e36614b98ddd
SLoban <sloban@ibagroup.eu>
2019-06-17 23:47:35 +0300

    implemented table view task

---
+++ Week1StoryboardNavigation/Services/Services.swift
@@ -0,0 +1,19 @@
+//
+//  DataProvider.swift
+//  Week1StoryboardNavigation
+//
+//  Created by Admin on 6/17/19.
+//  Copyright © 2019 SLoban. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+class Services {
+    
+    static var dataProvider: DataProvider = {
+        return DataProvider(count: 20)
+    }()
+    
+}
